54ea7c306fbfba67080efd194da3ac3e2fdd1f2a
GabCopriva <contatogabrielcopriva@gmail.com>
2023-02-08 16:12:26 -0300

    Exercicio java 4

--- pacotejava/Exercicio_08_02_Quarto.java
+++ pacotejava/Exercicio_08_02_Quarto.java
@@ -1,10 +1,35 @@
 package pacotejava;
 
+import java.util.Scanner;
+
 public class Exercicio_08_02_Quarto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Exercício 4 do dia 08.02.23
+		
+		int A,B,C;
+		int R,S,D;
+		double resultadoR;
+		double resultadoS,resultadoD;
+		
+		
+	    Scanner leia = new Scanner (System.in);
+	    
+	    System.out.println("\nEntre com o valor da letra A:  ");
+		A = leia.nextInt();
+		System.out.println("\nEntre com o valor da letra B:  ");
+		B = leia.nextInt();
+		System.out.println("\nEntre com o valor da letra C:  ");
+		C = leia.nextInt();
+		
+		
+		resultadoR= Math.pow(A, B);
+		resultadoS= Math.pow(B, C);
+		resultadoD = resultadoR+resultadoS / 2;
+		System.out.println(resultadoD);
+		System.out.println("\nO resultado de D=R+S/2 é de: " +resultadoD);
 
+		
 	}
 
 }
